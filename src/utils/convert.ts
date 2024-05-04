118244e95a201e958620979be1b429209a585721
Hanse Kim <procrustes5@gmail.com>
2024-05-04 13:35:56 +0900

    Refactoring

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,7 +1,10 @@
 import type { Piece, ChessSquare } from './utils/type'
 
+// 座標
 export const file = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 export const rank = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// 駒情報
 export const piece: Piece[] = [
   { code: 'k', name: 'King' },
   { code: 'q', name: 'Queen' },
@@ -12,6 +15,7 @@ export const piece: Piece[] = [
   { code: '', name: 'none' }
 ]
 
+// 初期位置設定
 export const initSquare = (piece: Piece, teamColor: computed<{ myColor: string, oppoColor: string }>) => [
   // King
   { file: 4, rank: 7, piece: piece[0], color: teamColor.value.oppoColor },
@@ -54,6 +58,7 @@ export const initSquare = (piece: Piece, teamColor: computed<{ myColor: string, 
   { file: 7, rank: 1, piece: piece[5], color: teamColor.value.myColor }
 ]
 
+// ルール説明
 export const ja_rule_explanation: { code: string, content: string }[] = [
   { code: 'k', content: 'キングは1歩だけ八方に動きます。\n＜将棋の王将＞', name: 'キング' },
   { code: 'q', content: 'クイーンは八方に動く最強の駒です。\n＜将棋の飛車＋角行＞', name: 'クイーン' },
@@ -61,5 +66,5 @@ export const ja_rule_explanation: { code: string, content: string }[] = [
   { code: 'b', content: 'ビショップは斜めに動きます。\n＜将棋の角行＞', name: 'ビショップ' },
   { code: 'n', content: 'L字ジャンプします。ナイトだけが他の駒を飛びこえます。\n＜八方桂馬＞', name: 'ナイト' },
   { code: 'p', content: '前に1歩進みます。＜将棋の歩兵に相当＞\n最初の位置から2歩動いてもよい。\n駒を取る時だけななめ前！＜将棋と違う！＞', name: 'ポーン' },
-  { code: '', content: '' },
+  { code: '', content: '移動する駒を選択してください' },
 ]
